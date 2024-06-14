2d3cbfe8636b1ed33dc9bf84ce42a6aadbb86b3b
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2024-06-14 19:41:08 +0300

    Add Developer Skills.

--- Developer-Skills/script.js
+++ Developer-Skills/script.js
@@ -1,3 +1,19 @@
 'use strict';
 
-const x = 23;
+/**
+ * in this section i add node.js , live server (hot reload) and prettier format and learn other skills about solving and formating by prettier.
+ */
+
+// Task 1
+const forecastedDayOne = [17, 21, 23];
+const forecastedDayTwo = [12, 5, -5, 0, 4];
+function printForecast(arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+  console.log('... ' + str);
+}
+
+printForecast(forecastedDayOne);
+printForecast(forecastedDayTwo);
